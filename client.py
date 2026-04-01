05c6d83371329ab0d3c706af18dcae34a3cdbf0b
Italo Siqueira Lima <italosiqueira@gmail.com>
2026-03-31 21:07:01 -0300

    feat: implementa uma CLI do Agente (operação única)

--- client.py
+++ client.py
@@ -11,33 +11,36 @@ dotenv.load_dotenv()  # Carrega as variáveis de ambiente do arquivo .env
 # Pasta do projeto
 projeto_dir = os.path.dirname(__file__)
 
-async def main():
-    client = MultiServerMCPClient(
-        {
-            "clinic": {
-                "transport": "stdio",  # Local subprocess communication
-                "command": "python",
-                # Absolute path to your math_server.py file
-                "args": [os.path.join(projeto_dir, "clinica_mcp", "clinica_mcp.py")],
-            }
+client = MultiServerMCPClient(
+    {
+        "clinic": {
+            "transport": "stdio",  # Local subprocess communication
+            "command": "python",
+            # Absolute path to your math_server.py file
+            "args": [os.path.join(projeto_dir, "clinica_mcp", "clinica_mcp.py")],
         }
-    )
+    }
+)
 
+async def load_agent():
     tools = await client.get_tools()
-    agent = create_agent(
+    return create_agent(
         "gpt-4o-mini",
         tools  
     )
 
+async def responder_pergunta(pergunta: str):
+
+    agent = await load_agent()
+
     paciente_response = await agent.ainvoke(
-        {"messages": [{"role": "user", "content": "Busque o paciente com CPF 99999999999"}]}
+        {"messages": [{"role": "user", "content": pergunta}]}
     )
 
     output = paciente_response.get('messages')[3].content
     output_structured = paciente_response.get('messages')[2].artifact['structured_content']
 
-    print(f"## Resposta do agente ##:\n {output}\n\n")
-    print(f"## Objeto Estruturado ##:\n {output_structured}\n\n")
+    return output, output_structured
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    print(asyncio.run(responder_pergunta("Busque o paciente com CPF 11111111111")))
---
+++ main.py
@@ -0,0 +1,21 @@
+import asyncio
+import client
+
+def efetua_pergunta():
+    print('---\n')
+    return input('# PERGUNTA (digite "sair" para encerrar) \n')
+
+async def inicia_chat():
+    print('### BEM-VINDO AO AGENTE CLÍNICO. ###')
+
+    prompt = efetua_pergunta()
+
+    while prompt.strip().lower() != 'sair':
+        resposta, obj = await client.responder_pergunta(prompt)
+        print(f"\n# Resposta:\n {resposta}\n")
+        prompt = efetua_pergunta()
+    
+    print('### OBRIGADO POR USAR O AGENTE CLÍNICO! ATÉ LOGO! ###')
+
+if __name__ == "__main__":
+    asyncio.run(inicia_chat())
