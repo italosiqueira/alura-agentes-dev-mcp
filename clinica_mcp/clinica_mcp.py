e0883fbe15391cbfd27d46754ecfd7c78c2306c7
Italo Siqueira Lima <italosiqueira@gmail.com>
2026-03-31 23:49:39 -0300

    feat: implementa nova tool para cadastro de pacientes

--- clinica_mcp/clinica_mcp.py
+++ clinica_mcp/clinica_mcp.py
@@ -14,5 +14,20 @@ async def buscar_paciente_por_cpf(cpf: str):
     response = requests.get(f"{url}/pacientes/{cpf}")
     return response.json()
 
+@mcp.tool
+async def cadastrar_paciente(nome: str, cpf: str, telefone: str, convenio: str):
+    """Cadastra um novo paciente."""
+    payload = {
+        "nome": nome,
+        "cpf": cpf,
+        "telefone": telefone,
+        "convenio": convenio
+    }
+    response = requests.post(f"{url}/pacientes", json=payload)
+    if response.status_code == 200:
+        return response.json()["mensagem"]
+    else:
+        return response.json().detail[0]
+
 if __name__ == "__main__":
     mcp.run(transport="stdio")
